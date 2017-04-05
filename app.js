2d4942d36a7ace884f8dec66dec76048e93d7056
ivan.matvieiev <ivan_matvieiev_tp@bmc.com>
2017-04-05 12:33:24 +0300

    [DIRECT] Port argv

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const koa = require("koa");
 const router = require("./router");
 
 const app = koa();
-app.listen(3000);
 
+let portIndex = process.argv.findIndex(argument => argument.includes('port'));
+
+let port = process.argv[ portIndex + 1 ];
+if (portIndex === -1) {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port} port`);
+});
 app.use(router.routes()).use(router.allowedMethods());
